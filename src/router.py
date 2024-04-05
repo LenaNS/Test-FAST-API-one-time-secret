89dd0988f7465a15bf14e8c3a373a1408075827b
LenaNS <71088004+LenaNS@users.noreply.github.com>
2024-04-05 15:22:45 +0800

    Update router.py

--- src/router.py
+++ src/router.py
@@ -1,6 +1,4 @@
-from fastapi import APIRouter, Depends
-from src.schema import SSecretAdd
-from typing import Annotated
+from fastapi import APIRouter
 from src.repository import SecretRepository
 
 
@@ -11,11 +9,29 @@ router = APIRouter(
 
 @router.post("/generate")
 async def get_secret(secret: str, secret_phrase: str) -> dict:
+    """Принимает secret и secret_phrase и отдает secret_key по которому этот secret можно получить.
+
+    Args:
+        secret (str): секрет
+        secret_phrase (str): кодовая фраза
+
+    Returns:
+        dict: secret_key
+    """
     secret_key = await SecretRepository.add_one(secret, secret_phrase)
     return {"secret_key": secret_key}
 
 
 @router.get("/secrets/{secret_key}")
 async def get_secret(secret_key: str, secret_phrase: str) -> dict:
+    """Принимает на вход secret_key и secret_phrase и отдает secret
+
+    Args:
+        secret_key (str): секретный ключ
+        secret_phrase (str): кодовая фраза
+
+    Returns:
+        dict: секрет
+    """
     secret = await SecretRepository.get_secret(secret_key=secret_key, secret_phrase=secret_phrase)
     return {"secret": secret}
