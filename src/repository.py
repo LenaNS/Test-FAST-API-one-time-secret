84c4c747f7805743e8b32e3164fd2643e02c789f
LenaNS <71088004+LenaNS@users.noreply.github.com>
2024-04-05 15:12:53 +0800

    Update repository.py

--- src/repository.py
+++ src/repository.py
@@ -10,8 +10,21 @@ CODER = Fernet(b'gHXY2sgsp-WanAEhugFf9R9LoFS5ZPZLSSsTjG8sdqM=')
 
 
 class SecretRepository: 
+
+    
     @classmethod
     async def add_one(cls, secret: str, secret_phrase: str) -> str:
+        """ На основе secret и secret_phrase
+            создает запись в бд, secret перед добавление шифруется,  
+            secret_phrase - хэшируется
+
+        Args:
+            secret (str): секрет
+            secret_phrase (str): секретная фраза
+
+        Returns:
+            str: возвращает секретный ключ
+        """
         async with session_local() as session:
             secret_phrase = hashlib.pbkdf2_hmac(hash_name='sha256', password=secret_phrase.encode(), salt=SALT.encode(), iterations=100000).hex()
             secret = CODER.encrypt(secret.encode())
@@ -21,13 +34,16 @@ class SecretRepository:
             await session.commit()
             return one_secret.secret_key
         
-    # @classmethod
-    # async def delete_secret(cls, secret_key: str, secret_phrase: str) -> str:
-    #     async with session_local() as session:
-    #         return None
-        
     @classmethod
     async def get_secret(cls, secret_key: str, secret_phrase: str) -> str:
+        """По secret_key и secret_phrase получаем secret и удаляем запись из бд
+
+        Args:
+            secret_key (str): секретный ключ
+            secret_phrase (str): секртная фраза
+        Returns:
+            str: расшифрованый секрет
+        """
         async with session_local() as session:
             secret_phrase = hashlib.pbkdf2_hmac(hash_name='sha256', password=secret_phrase.encode(), salt=SALT.encode(), iterations=100000).hex()
             query = select(SecretsOrm).where(and_(SecretsOrm.secret_key == secret_key, SecretsOrm.secret_phrase == secret_phrase))
@@ -39,4 +55,5 @@ class SecretRepository:
             await session.flush()
             await session.commit()
             return CODER.decrypt(secret.secret)
+            
         
